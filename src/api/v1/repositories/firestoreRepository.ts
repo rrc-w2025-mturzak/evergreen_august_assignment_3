9cbbf8b09d7bdb58dcf2736d2ef87c0b85d8dd74
August <mturzak@rrc.ca>
2026-02-25 15:51:27 -0600

    added addEvent to firestore repository

--- src/api/v1/repositories/firestoreRepository.ts
+++ src/api/v1/repositories/firestoreRepository.ts
@@ -1,1 +1,24 @@
+import { db } from "../config/firebaseConfig";
+import { DocumentReference, QuerySnapshot } from "firebase-admin/firestore";
+import { EventModel } from "../models/eventModel";
+import { EventCreateRequest } from "../models/eventCreateRequestModel";
+import { EventDTO } from "../models/eventDTO";
+
+export const addEvent = async (item:EventCreateRequest): Promise<string> => {
+
+    const docRef: DocumentReference = db.collection("events").doc();
+
+    const itemEntity: EventModel = {
+        name: item.name,
+        date: new Date,
+        capacity: item.capacity,
+        registrationCount: item.registrationCount,
+        status: item.status,
+        category: item.category,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    }
+    await docRef.set(itemEntity);
+    return docRef.id;
+};
 
