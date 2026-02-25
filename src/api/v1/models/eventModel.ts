f7a0298114b171939848b3097cfa1bddf337a25c
August <mturzak@rrc.ca>
2026-02-25 15:34:57 -0600

    added eventModel

---
+++ src/api/v1/models/eventModel.ts
@@ -0,0 +1,10 @@
+export interface EventModel {
+    name: string;
+    date: Date;
+    capacity: number;
+    registrationCount: number;
+    status: string;
+    category: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
