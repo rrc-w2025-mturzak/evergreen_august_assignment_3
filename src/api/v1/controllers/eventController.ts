99bde1465eb37f0bba24d85065d566d52ac2f269
August <mturzak@rrc.ca>
2026-02-25 15:22:38 -0600

    added health chekup to contoller and functions to be reformated for events

--- src/api/v1/controllers/eventController.ts
+++ src/api/v1/controllers/eventController.ts
@@ -1,1 +1,68 @@
+import { Request, Response } from "express";
+import { HTTP_STATUS } from "../../../constants/httpConstants";
+// import { successResponse } from "../models/responseModel";
+// import { createNewProduct, getProductByIdAsync, getAllProducts, updateProductById, deleteProductById } from "../services/eventService";
+// import { ProductCreateRequest } from "../models/eventCreateRequestModel";
 
+export const healthData = (req: Request, res: Response) => {
+    res.status(HTTP_STATUS.OK).json({
+        status: HTTP_STATUS.OK,
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString(),
+        version: "1.0.0"
+    });
+};
+
+// export const createProduct = async (req: Request, res: Response) => {
+//     const requestProduct: ProductCreateRequest = {
+//         name: req.body.name,
+//         sku: req.body.sku,
+//         quantity: req.body.quantity,
+//         price: req.body.price,
+//         category: req.body.category
+//     }
+//     let result = await createNewProduct(requestProduct)
+//     res.status(HTTP_STATUS.CREATED).send(result)
+// }
+
+// export const getProductById = async (req: Request, res: Response) => {
+//     try {
+//         let id = req.params.id as string;
+//         let results = await getProductByIdAsync(id)
+
+//         res.status(HTTP_STATUS.OK).json(successResponse(results, "Product retrieved"))
+//     } catch (error) {
+//         res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({ message: "Internal Server Error"})
+//     }
+// }
+
+// export const getAllProduct = async (req: Request, res: Response) => {
+//     try {
+//         const products = await getAllProducts();
+//         res.status(HTTP_STATUS.OK).json(successResponse(products, "Products retrieved"))
+//     } catch (error) {
+//         res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({ message: "Internal Server Error"})
+//     }
+// }
+
+// export const updateProductByIdAsync = async (req: Request, res: Response) => {
+//     let id: string = req.params.id as string; 
+//     let request: ProductCreateRequest = {
+//         name: req.body.name,
+//         sku: req.body.sku,
+//         quantity: req.body.quantity,
+//         price: req.body.price,
+//         category: req.body.category
+//     }
+
+//     await updateProductById(id, request)
+
+//     res.status(HTTP_STATUS.NO_CONTENT).send(`Product ${id} was updated`);
+// }
+
+// export const deleteProductByIdAsync = async (req: Request, res: Response) => {
+//     let id = req.params.id as string;
+//     await deleteProductById(id)
+
+//     res.status(HTTP_STATUS.NO_CONTENT).send(`Product ${id} was deleted`);
+// }
