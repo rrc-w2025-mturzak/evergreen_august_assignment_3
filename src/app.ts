5018700b00685b62613d53783fd5a6105b6cd4fb
August <mturzak@rrc.ca>
2026-02-25 18:26:27 -0600

    connected routing to app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express, {Express} from "express";
-// import eventRouter from "./api/v1/routes/eventRoutes";
+import eventRouter from "./api/v1/routes/eventRoutes";
 
 const app: Express = express();
 
 app.use(express.json());
 
-app.use("/api/v1/");
+app.use("/api/v1/", eventRouter);
 
 export default app;
+
