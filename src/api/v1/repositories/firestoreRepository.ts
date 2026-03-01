ae5d82e4d54400b7be01e61add990194125bfa15
August <mturzak@rrc.ca>
2026-02-28 18:47:16 -0600

    Final commit

--- src/api/v1/repositories/firestoreRepository.ts
+++ src/api/v1/repositories/firestoreRepository.ts
@@ -6,7 +6,7 @@ import { EventDTO } from "../models/eventDTO";
 
 export const addEvent = async (item:EventCreateRequest): Promise<string> => {
 
-    const docRef: DocumentReference = db.collection("events").doc("evt_000009");
+    const docRef: DocumentReference = db.collection("events").doc();
 
     const itemEntity: EventModel = {
         name: item.name,
