c95b0d98dbe514f413a184adbcc3d1b405602b4f
August <mturzak@rrc.ca>
2026-03-26 22:05:56 -0500

    added base cors config

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,14 @@ dotenv.config();
 import eventRouter from "./api/v1/routes/eventRoutes";
 import morgan from "morgan";
 import { getHelmetConfig } from "./api/v1/config/helmetConfig";
+import cors from "cors";
+import { getCorsOptions } from "./api/v1/config/corsConfig";
 
 const app: Express = express();
 
 app.use(getHelmetConfig());
+
+app.use(cors(getCorsOptions()));
 
 app.use(express.json());
 
